fe15b09ef41ffd0ae9368f28f80e69d9ee992568
Kazuya.N <aibotaro@gmail.com>
2016-07-28 13:28:16 +0900

    Add send accelaration with OUTPUT_TEAPOT

--- Arduino/MPU6050/examples/MPU6050_DMP6/MPU6050_DMP6.ino
+++ Arduino/MPU6050/examples/MPU6050_DMP6/MPU6050_DMP6.ino
@@ -97,7 +97,7 @@ MPU6050 mpu;
 // from the FIFO. Note this also requires gravity vector calculations.
 // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
 // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-#define OUTPUT_READABLE_YAWPITCHROLL
+// #define OUTPUT_READABLE_YAWPITCHROLL
 
 // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
 // components with gravity removed. This acceleration reference frame is
@@ -114,7 +114,7 @@ MPU6050 mpu;
 
 // uncomment "OUTPUT_TEAPOT" if you want output that matches the
 // format used for the InvenSense teapot demo
-//#define OUTPUT_TEAPOT
+#define OUTPUT_TEAPOT
 
 
 
@@ -140,7 +140,7 @@ float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 // packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+uint8_t teapotPacket[20] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 
 
 
@@ -363,8 +363,14 @@ void loop() {
             teapotPacket[7] = fifoBuffer[9];
             teapotPacket[8] = fifoBuffer[12];
             teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+            teapotPacket[10] = fifoBuffer[28];
+            teapotPacket[11] = fifoBuffer[29];
+            teapotPacket[12] = fifoBuffer[32];
+            teapotPacket[13] = fifoBuffer[33];
+            teapotPacket[14] = fifoBuffer[36];
+            teapotPacket[15] = fifoBuffer[37];
+            Serial.write(teapotPacket, 20);
+            teapotPacket[17]++; // packetCount, loops at 0xFF on purpose
         #endif
 
         // blink LED to indicate activity
